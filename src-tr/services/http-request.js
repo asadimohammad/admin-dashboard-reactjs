3f4596a9cfab2eb351d0b46dcd1289767ab64b96
Mohammad Asaadi <67.asadi@gmail.com>
2023-08-18 10:42:02 +0330

    change repo files

--- src-tr/services/http-request.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-const BASE_URL = 'https://react-mini-projects-api.classbon.com'
-// const BASE_URL = 'https://dummyjson.com/auth/login'
-
-export const httpService = axios.create({
-    baseURL : BASE_URL
-})
